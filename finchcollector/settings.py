1a1e6a3a322d0643c8763ceff6eceaafb36e5f34
tysonwells <tysonwells370@gmail.com>
2021-11-29 14:42:09 -0600

    create urls.py in main aap

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,10 @@ WSGI_APPLICATION = 'finchcollector.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'finchcollector',
+	}
 }
 
 
